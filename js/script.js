b5578ec8aeed292c8dd1cf251e2bb2d93210bd82
yaelcabrera03 <yaelcabrera723@gmail.com>
2026-04-28 16:15:46 -0600

    input: Primera version de sesion

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function login() {
+    let divMensaje = document.querySelector('#mensaje');
+    let usuario = document.querySelectorAll('.entradaTexto')[0].value;
+    let contra = document.querySelectorAll('.entradaTexto')[1].value;
+
+    if (usuario == "admin" && contra == "123") {
+        localStorage.setItem("usuario", usuario);
+        window.location.href = "../pages/principal.html";
+    } else {
+        divMensaje.innerHTML = "credenciales incorrectas";
+        divMensaje.style.display = "inline-block";
+    }
+}
+
+const btnIngresar = document.getElementById('btnIngresar');
+btnIngresar.addEventListener("click", login);
+
+
+let usuarioGuardado = localStorage.getItem("usuario");
+
+if (usuarioGuardado) {
+    let div = document.getElementById("mensajeBienvenida");
+
+    if (div) {
+        div.innerHTML = "Bienvenido " + usuarioGuardado;
+        div.style.display = "block";
+    }
+}
+function logout() {
+    localStorage.removeItem("usuario");
+    window.location.href = "../pages/principal.html";
+}
