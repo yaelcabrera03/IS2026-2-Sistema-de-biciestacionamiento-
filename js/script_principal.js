e31a0d81005ca0f7d61c1e686dba2520836bf6a4
Eduardo <salinas.eduardo17a@gmail.com>
2026-05-05 18:50:22 -0600

    Embebí el video en la seccion de ayuda

--- js/script_principal.js
+++ js/script_principal.js
@@ -2,8 +2,15 @@ let usuario = localStorage.getItem("usuario");
 
 document.getElementById("msjBienvenida").innerHTML = "Bienvenido, " + usuario;
 document.getElementById("cerrarSesion").addEventListener("click",cerrarSesion);
+document.getElementById("opAyuda").addEventListener("click",redirigirAyuda);
 function cerrarSesion(){
     localStorage.removeItem("usuario");
     window.location.href="../pages/principal.html";
 }
+function redirigirAyuda(){
+    window.location.href="../pages/QA.html";
+}
+
+
+
 
