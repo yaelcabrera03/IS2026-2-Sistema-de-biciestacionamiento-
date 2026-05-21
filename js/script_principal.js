0ac6cff7f7ebe2431d6d20da9548f03e2d509087
Eduardo <salinas.eduardo17a@gmail.com>
2026-05-21 15:14:44 -0600

    añadi la pagina de ayuda

--- js/script_principal.js
+++ js/script_principal.js
@@ -4,7 +4,7 @@ document.getElementById("msjBienvenida").innerHTML = "Bienvenido, " + usuario;
 document.getElementById("cerrarSesion").addEventListener("click",cerrarSesion);
 document.getElementById("opAyuda").addEventListener("click",redirigirAyuda);
 document.getElementById("opcion_Cuenta").addEventListener("click",redirigirCuenta)
-document.getElementById("opcionHeader").addEventListener("click",redirigirPrincipal);
+
 function cerrarSesion(){
     localStorage.removeItem("usuario");
     window.location.href="../index.html";
@@ -15,9 +15,7 @@ function redirigirAyuda(){
 function redirigirCuenta(){
     window.location.href = "cuenta.html";
 }
-function redirigirPrincipal(){
-    window.location.href="principal.html";
-}
+
 
 
 
